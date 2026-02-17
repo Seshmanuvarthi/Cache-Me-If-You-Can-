5c6d9d6d3dc9d139de7aa9b31351a6d32d76a4c2
Sesh Manuvarthi <seshmanuvarthi27@gmail.com>
2026-02-17 20:02:19 +0530

    Added backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://cache-me-if-you-can-a3gi.onrender.com',
         changeOrigin: true
       }
     }
